966139ac1245ff03478944ce8781e8d589ac1ff2
HowardLight <howardlight@protonmail.com>
2022-11-13 19:14:24 +0000

    Added SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,40 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Fragment } from 'react';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
+      <NextSeo
+        title="Project Movies"
+        description="Explore the latest and most popular movies using Project Movies! Discover details never heard of about your favorite actors! Find your next favorite TV Show using Project Movies!"
+        canonical="https://project-movies.vercel.app"
+        openGraph={{
+          url: 'https://project-movies.vercel.app',
+          title: 'Project Movies',
+          description: "Explore the latest and most popular movies using Project Movies! Discover details never heard of about your favorite actors! Find your next favorite TV Show using Project Movies!",
+          images: [
+            {
+              url: 'https://www.example.com/og-image01.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+              type: 'image/jpeg',
+            },
+            {
+              url: 'https://www.example.com/og-image02.jpg',
+              width: 900,
+              height: 800,
+              alt: 'Og Image Alt Second',
+              type: 'image/jpeg',
+            },
+            { url: 'https://www.example.com/og-image03.jpg' },
+            { url: 'https://www.example.com/og-image04.jpg' },
+          ],
+          site_name: 'Project Movies',
+        }}
+      />
       <Component {...pageProps} />
     </Fragment>
   )
