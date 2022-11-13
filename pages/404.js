6663e0e7cab844408592207c24c2e93ed0b94b47
HowardLight <howardlight@protonmail.com>
2022-11-13 19:32:09 +0000

    Converted Page404 to 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,16 @@
-import { Navbar } from "./Navbar";
 import { Fragment } from "react";
+import { Navbar } from "../components/Navbar";
 
-export const Page404 = () => {
+
+export default function Custom404() {
     return (
-        <Fragment>
+        <div className="w-[100vw] h-[100vh] flex flex-col">
             <Navbar />
-            <main className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+            <main className="flex flex-col justify-center items-center grow">
                 <p className="text-3xl text-neutral-200">404</p>
                 <p className="text-lg text-neutral-500">It seems what you&apos;re looking for is not found</p>
             </main>
-        </Fragment>
+        </div>
 
     );
 };
