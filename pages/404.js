f68db30aa40bc46980098c1314ab9cf4f98d2564
HowardLight <howardlight@protonmail.com>
2022-11-13 19:35:08 +0000

    Added SEO

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,21 @@
+import { NextSeo } from "next-seo";
 import { Fragment } from "react";
 import { Navbar } from "../components/Navbar";
 
 
 export default function Custom404() {
     return (
-        <div className="w-[100vw] h-[100vh] flex flex-col">
-            <Navbar />
-            <main className="flex flex-col justify-center items-center grow">
-                <p className="text-3xl text-neutral-200">404</p>
-                <p className="text-lg text-neutral-500">It seems what you&apos;re looking for is not found</p>
-            </main>
-        </div>
-
+        <Fragment>
+            <NextSeo
+                title="404 - Project Movies"
+            />
+            <div className="w-[100vw] h-[100vh] flex flex-col">
+                <Navbar />
+                <main className="flex flex-col justify-center items-center grow">
+                    <p className="text-3xl text-neutral-200">404</p>
+                    <p className="text-lg text-neutral-500">It seems what you&apos;re looking for is not found</p>
+                </main>
+            </div>
+        </Fragment>
     );
 };
