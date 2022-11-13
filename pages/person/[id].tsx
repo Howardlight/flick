7aa188bab5eb99ccc99a80dc0fd09b2ec1da3f20
HowardLight <howardlight@protonmail.com>
2022-11-13 19:23:06 +0000

    Fixed Minor Issue with passed props

--- pages/person/[id].tsx
+++ pages/person/[id].tsx
@@ -136,7 +136,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     return {
         props: {
             data: data,
-            status: request.status
+            requestStatus: request.status
         }
     }
 }
