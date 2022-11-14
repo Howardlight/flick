43314c7b11a4fada2cc51912a7582b5ecfe328f3
HowardLight <howardlight+github@protonmail.com>
2022-11-14 22:39:05 +0200

    Created Episode Type

---
+++ types/Episode.ts
@@ -0,0 +1,18 @@
+import { Crew } from "./Crew";
+
+export interface Episode {
+    air_date:        Date;
+    episode_number:  number;
+    id:              number;
+    name:            string;
+    overview:        string;
+    production_code: string;
+    runtime:         number;
+    season_number:   number;
+    show_id:         number;
+    still_path:      string;
+    vote_average:    number;
+    vote_count:      number;
+    crew:            Crew[];
+    guest_stars:     Crew[];
+}
