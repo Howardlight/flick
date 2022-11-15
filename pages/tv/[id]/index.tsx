fd774cd1b61039c937a560db479ad8ed0f176518
HowardLight <howardlight+github@protonmail.com>
2022-11-15 11:32:53 +0200

    Updated Margins

--- pages/tv/[id]/index.tsx
+++ pages/tv/[id]/index.tsx
@@ -70,8 +70,8 @@ export default function TVShowPage({ data, mediaType, requestStatus }: { data: T
                 <br />
                 
                 <SeasonsWidget seasons={data.seasons} TVID={data.id} />
-                <CastWidget id={data.id} mediaType={mediaType} className={"mt-10"} />
-                {data.created_by.length >= 1 ? <CreatorWidget creators={data.created_by} className={"mt-10"} /> : <div />}
+                <CastWidget id={data.id} mediaType={mediaType} className={"mt-4"} />
+                {data.created_by.length >= 1 ? <CreatorWidget creators={data.created_by} className={"mt-4"} /> : <div />}
             </div>
 
         </div>
