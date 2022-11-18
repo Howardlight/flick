ebabd7bbe66b9bc876c82d05a42c1796a74d5910
HowardLight <howardlight+github@protonmail.com>
2022-11-18 20:54:28 +0200

    Added Magnifier svg

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import { NextSeo } from "next-seo";
 import { Fragment } from "react";
 import { Navbar } from "../components/Navbar";
-
+import Magnifier from "../components/SVGComponents/Magnifier";
 
 export default function Custom404() {
     return (
@@ -12,6 +12,7 @@ export default function Custom404() {
             <div className="w-[100vw] h-[100vh] flex flex-col">
                 <Navbar />
                 <main className="flex flex-col justify-center items-center grow">
+                    <Magnifier fill={"rgb(115 115 115)"} viewBox="0 0 128 128" height={128} width={128} />
                     <p className="text-3xl text-neutral-200">404</p>
                     <p className="text-lg text-neutral-500">It seems what you&apos;re looking for is not found</p>
                 </main>
