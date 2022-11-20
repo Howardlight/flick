affbf5e7407222207c1b953761f420eb7b504603
HowardLight <howardlight+github@protonmail.com>
2022-11-20 11:21:03 +0200

    Created Comment text func

--- pages/movie/[id]/index.tsx
+++ pages/movie/[id]/index.tsx
@@ -84,8 +84,24 @@ export default function MoviePage({ data, mediaType, requestStatus }: { data: Mo
                 <br />
 
                 <CastWidget id={data.id} mediaType={mediaType} />
-            </div>
+//TODO: Check Performance and use callback or memoization
+const Comment = ({ text, className }: { text: string, className?: string }): React.ReactElement => {
 
+
+    // logic clarification: is showMoreButton needed?
+    // yes, substring and button. 
+    // no, dump the bio
+    const [showMore, setShowMore] = useState(false);
+    useEffect(() => {
+        if (text.length > 250) setShowMore(true);
+
+    }, [text.length])
+
+    if (!text || text == "") return <Fragment></Fragment>;
+    return (
+        <div className={`${className}`}>
+            <p className="inline">{showMore ? `${text.substring(0, 250)}` : text}</p>
+            {showMore ? <div className="flex items-center justify-center z-10 relative bottom-6 backdrop-blur-sm hover:cursor-pointer hover:contrast-150" onClick={() => setShowMore(false)}>Show more</div> : <Fragment />}
         </div>
     )
 }
