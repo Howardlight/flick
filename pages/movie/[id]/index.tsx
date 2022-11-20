680bb1f1f61c80e79521018d0772ca702ce3a312
HowardLight <howardlight+github@protonmail.com>
2022-11-20 11:21:14 +0200

    Created Comments

--- pages/movie/[id]/index.tsx
+++ pages/movie/[id]/index.tsx
@@ -9,12 +9,20 @@ import Custom404 from "../../404";
 import { NextSeo } from "next-seo";
 import Placeholder from "../../../assets/MovieSVG.svg";
 import { isReleased } from "../../search/[...query]";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import useSWR, { SWRResponse } from "swr";
+import fetcher from "../../../Fetcher";
+import { ReviewResponse } from "../../../types/ReviewResponse";
+import { AvatarLoader } from "../../../AvatarLoader";
+import { ImageWithFallback } from "../../../components/ImageWithFallback";
+import Star from "../../../assets/Star.svg";
 
 //TODO: Add case for when The movie is not released yet
 export default function MoviePage({ data, mediaType, requestStatus }: { data: Movie, mediaType: string, requestStatus: number }) {
     // console.log(data);
 
+    //TODO: Add recommended Movies
+    //TODO: OR Add Similar Movies
     if (requestStatus != 200) return <Custom404 />;
     return (
         <div>
@@ -84,6 +92,66 @@ export default function MoviePage({ data, mediaType, requestStatus }: { data: Mo
                 <br />
 
                 <CastWidget id={data.id} mediaType={mediaType} />
+                {data.vote_count > 1 ? <Reviews movieID={data.id} /> : <Fragment />}
+            </div>
+
+        </div>
+    )
+}
+
+const Reviews = ({ movieID }: { movieID: number }) => {
+    const [page, setPage] = useState(1);
+    const { data, error }: SWRResponse<ReviewResponse, Error> = useSWR(`/api/getmoviereviews/${movieID}/${page}`, fetcher);
+
+    console.log(data);
+    if (!data && !error) return <p>Loading...</p>;
+    if (error) return <p>Error</p>;
+    return (
+        <div>
+            {
+                data!.results.map((review, index) => {
+                    return (
+                        <div className="bg-neutral-9 rounded-md mt-5 mb-5" key={`${review.id}-${index}`}>
+                            <div className="flex flex-row items-center gap-2">
+                                <ImageWithFallback
+                                    alt={`${review.author}'s Avatar`}
+                                    src={review.author_details.avatar_path ? review.author_details.avatar_path : Placeholder.src}
+                                    fallback={Placeholder.src}
+
+                                    //@ts-ignore
+                                    loader={AvatarLoader}
+                                    width={64}
+                                    height={64}
+                                    className={"rounded-md"}
+                                />
+                                <div>
+                                    <p className="text-lg font-medium">{review.author}</p>
+                                    <p className="text-base font-medium text-neutral-400">{moment(review.created_at).format("LL")}</p>
+                                </div>
+
+                                {review.author_details.rating ?
+                                    <div className="flex flex-row items-center grow justify-end text-2xl font-semibold gap-2">
+                                        <p>{review.author_details.rating} / 10</p>
+                                        <Image
+                                            src={Star.src}
+                                            alt={"Star"}
+                                            width={32}
+                                            height={32}
+                                            style={{ fill: "" }}
+                                        />
+                                    </div>
+                                    : <Fragment />
+                                }
+                            </div>
+                            <Comment className="text-neutral-300 mt-4 ml-1" text={review.content} />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
+
 //TODO: Check Performance and use callback or memoization
 const Comment = ({ text, className }: { text: string, className?: string }): React.ReactElement => {
 
@@ -105,6 +173,8 @@ const Comment = ({ text, className }: { text: string, className?: string }): Rea
         </div>
     )
 }
+
+
 
 const Metrics = ({ data, styles }: { data: Movie, styles: string }) => {
 
