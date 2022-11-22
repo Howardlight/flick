bee89393c537a7b701458ca113c75f47e450820f
HowardLight <howardlight+github@protonmail.com>
2022-11-22 13:18:15 +0200

    Added Bigger Substring for bigger screen

--- components/Reviews/Comment.tsx
+++ components/Reviews/Comment.tsx
@@ -1,10 +1,13 @@
 import { Fragment, useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 //TODO: Check Performance and use callback or memoization
 export const Comment = ({ text, className }: { text: string; className?: string; }): React.ReactElement => {
     // logic clarification: is showMoreButton needed?
     // yes, substring and button. 
     // no, dump the bio
+
+    const isMobile = useMediaQuery({ query: `(max-width: 768px)` });
 
 
     //TODO: Change substring size depending on breakpoints
@@ -19,7 +22,15 @@ export const Comment = ({ text, className }: { text: string; className?: string;
         return <Fragment></Fragment>;
     return (
         <div className={`${className}`}>
-            <p className="inline">{showMore ? `${text.substring(0, 250)}` : text}</p>
+            <p className="inline">
+                {
+                    showMore ?
+                        isMobile ?
+                            `${text.substring(0, 250)}`
+                            : `${text.substring(0, 750)}`
+                        : text
+                }
+            </p>
             {showMore ? <div className="flex items-center justify-center z-10 relative font-semibold bottom-6 backdrop-blur-sm hover:cursor-pointer hover:contrast-150" onClick={() => setShowMore(false)}>Show more</div> : <Fragment />}
         </div>
     );
