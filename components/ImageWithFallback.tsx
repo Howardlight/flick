c7504b577701d6010f5425dae8413419dc87fc3c
HowardLight <howardlight+github@protonmail.com>
2022-11-27 11:51:10 +0200

    Added TS Docs

--- components/ImageWithFallback.tsx
+++ components/ImageWithFallback.tsx
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react"
 import Image, { ImageProps } from "next/image"
 
-/* TODO: Finish this TsDocs
+/**
+ * **ImageWithFallback**
  * 
- * @param param0 
- * @returns 
+ * next/Image Component with a Fallback image for when SRC fails to load
+ * 
+ * @remarks
+ * This Component uses an internal state, so be weary of over using it as it is bad for performance.
+ * 
+ * @param fallback src of the fallback Image 
+ * @param alt Alt image of the element
+ * @param src Original SRC of the image which has a chance to fail
+ * @param props the remainder of next/image's props
+ * @returns ReactElement 
  */
 export const ImageWithFallback = ({ fallback, alt, src, ...props }: { fallback: any, alt: string, src: string, props: ImageProps }) => {
     const [error, setError] = useState(null)
