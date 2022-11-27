9bfe48e6b1c2b064d13c7b2313bf9b3a7988ea63
HowardLight <howardlight+github@protonmail.com>
2022-11-27 13:22:43 +0200

    Moved Common Rec Components to RecommendationsBase

--- components/Recommendations/Recommendations.tsx
+++ components/Recommendations/Recommendations.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import { PosterLoader } from "../PosterLoader";
+import { PosterLoader } from "../../PosterLoader";
 import moment from "moment";
 import Placeholder from "../assets/MovieSVG.svg";
 import useSWR, { SWRResponse } from "swr";
-import fetcher from "../Fetcher";
-import { MovieRecResponse } from "../types/GetMovieRecommendationsTypes";
+import fetcher from "../../Fetcher";
+import { MovieRecResponse } from "../../types/GetMovieRecommendationsTypes";
 import Link from "next/link";
+import { NoRecommendations, RecommendationsError, RecommendationSkeletons } from "./RecommendationsBase";
 
 export const Recommendations = ({ id }: { id: number; }) => {
 
@@ -43,48 +44,6 @@ export const Recommendations = ({ id }: { id: number; }) => {
                             </div>
                         );
                 })}
-            </div>
-        </div>
-    );
-};
-const RecommendationsError = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-[326px] w-auto">
-            <p className="text-lg text-neutral-100">Could not load Recommendations</p>
-            <p className="text-base text-neutral-400">Try again later</p>
-        </div>
-    );
-};
-const NoRecommendations = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-[326px] w-auto">
-            <p className="text-lg text-neutral-100">It seems there are no recommendations</p>
-            <p className="text-base text-neutral-400">{`:(`}</p>
-        </div>
-    );
-};
-const RecommendationSkeletons = () => {
-    return (
-        <div className="mt-4">
-            <div className="animate-pulse bg-neutral-200 w-4/12 h-2 mb-3 rounded-sm"></div>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 justify-items-center">
-                <RecommendationSkeleton />
-                <RecommendationSkeleton />
-                <RecommendationSkeleton />
-                <RecommendationSkeleton />
-                <RecommendationSkeleton />
-                <RecommendationSkeleton />
-            </div>
-        </div>
-    );
-};
-const RecommendationSkeleton = () => {
-    return (
-        <div className="mb-5 p-3">
-            <div className="animate-pulse bg-neutral-400 w-[163px] h-[245px] rounded-sm"></div>
-            <div className="mt-4 flex flex-col gap-1">
-                <div className="bg-neutral-600 animate-pulse w-3/6 h-2 rounded-md mr-1"></div>
-                <div className="bg-neutral-400 animate-pulse w-4/6 h-2 rounded-md mr-1"></div>
             </div>
         </div>
     );
