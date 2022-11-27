32f5205b48c12497e41fbc3091f224f67164d715
HowardLight <howardlight+github@protonmail.com>
2022-11-27 13:41:10 +0200

    Removed console log

--- pages/api/TV/getRecommendations/[...args].ts
+++ pages/api/TV/getRecommendations/[...args].ts
@@ -14,8 +14,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const slugs = req.query;
     const TVID = slugs.args![0];
     let data;
-    console.log("TV RECOMMENDATIONS");
-    console.log(`TVID: `, TVID);
 
     const request = await fetch(`https://api.themoviedb.org/3/tv/${TVID}/recommendations?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`);
     data = await request.json();
