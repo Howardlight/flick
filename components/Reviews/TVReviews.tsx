63c789a829578aaf204750c4fb3d1fe11eea824b
HowardLight <howardlight+github@protonmail.com>
2022-12-03 10:56:02 +0200

    Updated TVReviews

--- components/Reviews/TVReviews.tsx
+++ components/Reviews/TVReviews.tsx
@@ -11,6 +11,7 @@ import { NoReviewsFound, ReviewError, Reviews, ReviewSkeletons } from "./Reviews
 
 import Placeholder from "../../assets/MovieSVG.svg";
 import Star from "../../assets/Star.svg";
+import { Default, Mobile } from "../../Breakpoints";
 
 export const TVReviews = ({ tvID, className }: { tvID: number, className?: string }) => (
     <Reviews className={`${className}`} >
@@ -39,40 +40,93 @@ export const TVReviewsContent = ({ tvID }: { tvID: number; }) => {
     if (data?.results.length == 0) return <NoReviewsFound />;
     return (
         <div>
-            {data!.results.map((review, index) => {
-                return (
-                    <div className="bg-neutral-9 rounded-md mt-5 mb-5" key={`${review.id}-${index}`}>
-                        <div className="flex flex-row items-center gap-2">
-                            <ImageWithFallback
-                                alt={`${review.author}'s Avatar`}
-                                src={review.author_details.avatar_path ? review.author_details.avatar_path : Placeholder.src}
-                                fallback={Placeholder.src}
+            <Default>
+                <Fragment>
+                    {data!.results.map((tv, index) => {
+                        return (
+                            <Fragment key={`${tv.id}-${index}`}>
+                                <div className="flex flex-row gap-4 mt-6 mb-6" key={`${tv.id}-${index}`}>
+                                    <div className="w-auto h-auto mt-2">
+                                        <ImageWithFallback
+                                            alt={`${tv.author}'s Avatar`}
+                                            src={tv.author_details.avatar_path ? tv.author_details.avatar_path : Placeholder.src}
+                                            fallback={Placeholder.src}
 
-                                //@ts-ignore
-                                loader={AvatarLoader}
-                                width={64}
-                                height={64}
-                                className={"rounded-md"} />
-                            <div>
-                                <p className="text-lg font-medium">{review.author}</p>
-                                <p className="text-base font-medium text-neutral-400">{moment(review.created_at).format("LL")}</p>
-                            </div>
+                                            //@ts-ignore
+                                            loader={AvatarLoader}
+                                            width={64}
+                                            height={64}
+                                            className={"rounded-md w-16 h-16"} />
+                                    </div>
 
-                            {review.author_details.rating ?
-                                <div className="flex flex-row items-center grow justify-end text-2xl font-semibold gap-2">
-                                    <p>{review.author_details.rating} / 10</p>
-                                    <Image
-                                        src={Star.src}
-                                        alt={"Star"}
-                                        width={32}
-                                        height={32} />
+
+                                    <div className="w-5/6 border p-2 rounded-md border-neutral-400">
+                                        <div className="flex flex-row justify-between h-16">
+                                            <div className="flex flex-col">
+                                                <p>{tv.author}</p>
+                                                <p className="text-base font-medium text-neutral-400">{moment(tv.created_at).format("LL")}</p>
+                                            </div>
+                                            {tv.author_details.rating ?
+                                                <div className="flex flex-row items-center justify-center mr-4 text-2xl font-semibold gap-2">
+                                                    <p className="text-lg">{tv.author_details.rating} / 10</p>
+                                                    <Image
+                                                        src={Star.src}
+                                                        alt={"Star"}
+                                                        width={32}
+                                                        height={32} />
+                                                </div>
+                                                : <Fragment />}
+                                        </div>
+                                        <Comment className="" text={tv.content} />
+                                    </div>
                                 </div>
-                                : <Fragment />}
-                        </div>
-                        <Comment className="text-neutral-300 mt-4 ml-1" text={review.content} />
-                    </div>
-                );
-            })}
+                                {/* <div className="flex items-center justify-center">
+                                    <hr className="solid w-4/6 bg-neutral-400 text-neutral-400"></hr>
+                                </div> */}
+                            </Fragment>
+                        );
+                    })}
+                </Fragment>
+            </Default>
+            <Mobile>
+                <Fragment>
+                    {data!.results.map((review, index) => {
+                        return (
+                            <div className="bg-neutral-9 rounded-md mt-5 mb-5" key={`${review.id}-${index}`}>
+                                <div className="flex flex-row items-center gap-2">
+                                    <ImageWithFallback
+                                        alt={`${review.author}'s Avatar`}
+                                        src={review.author_details.avatar_path ? review.author_details.avatar_path : Placeholder.src}
+                                        fallback={Placeholder.src}
+
+                                        //@ts-ignore
+                                        loader={AvatarLoader}
+                                        width={64}
+                                        height={64}
+                                        className={"rounded-md"} />
+                                    <div>
+                                        <p className="text-lg font-medium">{review.author}</p>
+                                        <p className="text-base font-medium text-neutral-400">{moment(review.created_at).format("LL")}</p>
+                                    </div>
+
+                                    {review.author_details.rating ?
+                                        <div className="flex flex-row items-center grow justify-end text-2xl font-semibold gap-2">
+                                            <p className="text-lg">{review.author_details.rating} / 10</p>
+                                            <Image
+                                                src={Star.src}
+                                                alt={"Star"}
+                                                width={32}
+                                                height={32} />
+                                        </div>
+                                        : <Fragment />}
+                                </div>
+                                <Comment className="text-neutral-300 mt-4 ml-1" text={review.content} />
+                            </div>
+                        );
+                    })}
+                </Fragment>
+            </Mobile>
+
         </div>
     );
 };
