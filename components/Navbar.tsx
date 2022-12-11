2c4ea8197336d091040f731f5e7b5af25ef3f007
HowardLight <howardlight+github@protonmail.com>
2022-12-11 18:46:36 +0200

    Improved Logging in UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,22 +2,50 @@ import MovieSVG from "../assets/MovieSVG.svg";
 import Image from "next/image";
 import Link from "next/link";
 import { handleLogin, logout } from "../pages";
-import { useContext } from "react";
+import { Fragment, useContext, useState } from "react";
 import { UserContext } from "../pages/_app";
 import { User } from "../types/User";
 import { NextRouter, useRouter } from "next/router";
+import Spinner from "./SVGComponents/Spinner";
 
 export const Navbar = () => {
 
+    const [loading, setLoading] = useState(false);
 
     const user = useContext(UserContext);
     const router = useRouter();
     // console.log(`Navbar user: `, user);
 
+    async function handleNavbarLogin(router: NextRouter) {
+        setLoading(true);
+        handleLogin(router);
+    }
+
+
     function UserElement({ user, router }: { user: User, router: NextRouter }) {
-        if (!user) return <p>Loading...</p>;
-        if (user.isLoggedIn) return <button className="font-semibold hover:text-neutral-100" onClick={() => logout(router)}>Log out</button>
-        else return <button onClick={() => handleLogin(router)} className="font-semibold hover:text-neutral-100">Log in</button>
+        if (!user) return (
+            <button className="flex flex-row items-center gap-2 cursor-default rounded-md font-semibold">
+                <div className="animate-spin">
+                    <Spinner className={"w-8 h-auto"} />
+                </div>
+                Loading...
+            </button>);
+
+        if (user.isLoggedIn) return (
+            <button className="flex flex-row items-center gap-2 font-semibold hover:text-neutral-100" onClick={() => logout(router)}>
+                Log out
+            </button>);
+
+        else return (
+            <button onClick={() => handleNavbarLogin(router)} className="font-semibold hover:text-neutral-100">
+                {!loading ?
+                    "Log in"
+                    : <div className="flex flex-row items-center gap-2">
+                        <Spinner className="animate-spin w-8 h-auto" />
+                        Logging in...
+                    </div>
+                }
+            </button>);
     }
 
 
@@ -37,11 +65,15 @@ export const Navbar = () => {
                 </a>
             </Link>
             <div className="grow"></div>
-            <div className="flex flex-row gap-3 justify-around items-center">
-
+            <div className="flex flex-row gap-2 items-center">
 
                 <UserElement user={user} router={router} />
-
+                {user && user.isLoggedIn == true ?
+                    <div className="flex items-center justify-center w-8 h-auto rounded-full text-lg border-2 font-semibold border-red-600">
+                        {user.username?.charAt(0)}
+                    </div>
+                    : <Fragment />
+                }
 
             </div>
         </nav>
