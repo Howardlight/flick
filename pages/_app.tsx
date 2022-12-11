e8a6932cdf055f4fba570686c34e2bb44e1d057a
HowardLight <howardlight+github@protonmail.com>
2022-12-11 19:58:37 +0200

    Added NProgress Bar when routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,47 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { createContext, Fragment } from 'react';
+import { createContext, Fragment, useEffect, useState } from 'react';
 import { NextSeo } from 'next-seo';
-import { useRouter } from 'next/router';
+import { useRouter, Router, NextRouter } from 'next/router';
 import useUser from '../useUser';
 import { User } from '../types/User';
+import NProgress from "nprogress";
+import Head from 'next/head';
 
 const defaultUserContext: User = {
   isLoggedIn: false
 }
 
+function useNProgressBar(router: Router) {
+  useEffect(() => {
+
+    router.events.on("routeChangeStart", (url) => {
+      NProgress.start()
+    });
+
+    router.events.on("routeChangeComplete", (url) => {
+      NProgress.done()
+    });
+
+    // Router.events.on("routeChangeError", (url) => {
+    //   NProgress.done()
+    // });
+
+  }, [router])
+}
+
 export const UserContext = createContext<User>(defaultUserContext);
 function MyApp({ Component, pageProps }: AppProps) {
 
-  const router = useRouter();
+  // ProgressBar when routing
+  useNProgressBar(Router as unknown as Router);
+
+
+  // Get Session/Cookie if present
   const { user } = useUser({
     redirectTo: "",
   });
   console.log("user: ", user);
-
 
   return (
     <Fragment>
