9f5b03ec107759ef68cdc480db2ef0b00c2165ba
HowardLight <howardlight+github@protonmail.com>
2022-12-11 14:42:16 +0200

    Completed Log in Log out lifecycle

---
+++ pages/api/logout.ts
@@ -0,0 +1,13 @@
+import { IronSessionData } from "iron-session";
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { ironOptions } from "./login/login";
+
+//@ts-ignore
+export default withIronSessionApiRoute(logoutRoute, ironOptions)
+
+
+function logoutRoute(req: NextApiRequest, res: NextApiResponse, session: IronSessionData) {
+    req.session.destroy();
+    res.send({ ok: true });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { NextRouter, useRouter } from 'next/router';
 import { PopularTV } from '../components/Index/PopularTV';
 import { NextSeo } from 'next-seo';
 import moment from 'moment';
+import useUser from '../useUser';
 
 interface Token {
   status_message: string
@@ -28,47 +29,14 @@ interface accessToken {
 const Home: NextPage = () => {
 
   const router = useRouter();
-  async function login(router: NextRouter) {
-    const req = await fetch("/api/requestToken", {
-      method: "POST",
-      body: JSON.stringify({
-        redirect_to: "localhost:3000",
-      })
-    });
-    const data: Token = await req.json();
-    console.log(data);
-    console.log(req.status);
 
-    if (req.status == 200) window.open(`https://www.themoviedb.org/auth/access?request_token=${data.request_token}`, "_blank");
+  const { user } = useUser({
+    redirectTo: "",
+  });
 
-    setTimeout(async function () {
-      const accessTokenReq = await fetch(`/api/accessToken`, {
-        method: "POST",
-        body: JSON.stringify({
-          request_token: data.request_token
-        })
-      })
-      const accessTokenData = await accessTokenReq.json();
-      console.log(accessTokenData);
-
-      if (accessTokenReq.status == 200) {
-
-        const v3Req = await fetch("/api/convertV4SessionToV3", {
-          method: "POST",
-          body: JSON.stringify({
-            access_token: accessTokenData.access_token
-          })
-        })
-        const v3ReqData = await v3Req.json();
-        console.log(v3ReqData);
-
-      }
-
-    }, 15000);
+  console.log("user: ", user);
 
 
-
-  }
   return (
     <div>
       <NextSeo
@@ -77,6 +45,7 @@ const Home: NextPage = () => {
       <Navbar />
       <main className='bg-black mb-10'>
         <button onClick={() => login(router)}>Log in</button>
+        <button onClick={() => logout(router)}>Log out</button>
         <div className='flex flex-col justify-center h-[50vh] bg-black'>
           <p className="font-semibold text-neutral-100 self-center text-xl mb-5">All kinds of Shows that you&apos;ll enjoy</p>
           <SearchBar />
@@ -88,6 +57,61 @@ const Home: NextPage = () => {
       </main>
     </div>
   )
+}
+
+async function logout(router: NextRouter) {
+  await fetch("/api/logout");
+  router.reload();
+}
+
+async function login(router: NextRouter) {
+  const req = await fetch("/api/login/requestToken", {
+    method: "POST",
+    body: JSON.stringify({
+      redirect_to: "localhost:3000",
+    })
+  });
+  const data: Token = await req.json();
+  console.log(data);
+
+  if (req.status == 200) window.open(`https://www.themoviedb.org/auth/access?request_token=${data.request_token}`, "_blank");
+
+  setTimeout(async function () {
+    const accessTokenReq = await fetch(`/api/login/accessToken`, {
+      method: "POST",
+      body: JSON.stringify({
+        request_token: data.request_token
+      })
+    })
+    const accessTokenData = await accessTokenReq.json();
+    console.log(accessTokenData);
+
+    if (accessTokenReq.status == 200) {
+
+      const v3Req = await fetch("/api/login/convertV4SessionToV3", {
+        method: "POST",
+        body: JSON.stringify({
+          access_token: accessTokenData.access_token
+        })
+      })
+      const v3ReqData = await v3Req.json();
+      console.log(v3ReqData);
+
+
+
+      const loginReq = await fetch("/api/login/login", {
+        method: "POST",
+        body: JSON.stringify({
+          session_id: v3ReqData.session_id
+        })
+      })
+
+    }
+
+
+
+
+  }, 15000);
 }
 
 //TODO: Finish autocomplete in the future
