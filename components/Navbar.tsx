be6fd5846ef0578a5512de2c96e62c208268b748
HowardLight <howardlight+github@protonmail.com>
2022-12-11 18:15:32 +0200

    Added Logging in logging out

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,26 @@
 import MovieSVG from "../assets/MovieSVG.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { handleLogin, logout } from "../pages";
+import { useContext } from "react";
+import { UserContext } from "../pages/_app";
+import { User } from "../types/User";
+import { NextRouter, useRouter } from "next/router";
 
 export const Navbar = () => {
+
+
+    const user = useContext(UserContext);
+    const router = useRouter();
+    // console.log(`Navbar user: `, user);
+
+    function UserElement({ user, router }: { user: User, router: NextRouter }) {
+        if (!user) return <p>Loading...</p>;
+        if (user.isLoggedIn) return <button className="font-semibold hover:text-neutral-100" onClick={() => logout(router)}>Log out</button>
+        else return <button onClick={() => handleLogin(router)} className="font-semibold hover:text-neutral-100">Log in</button>
+    }
+
+
     return (
         <nav className="flex flex-row pt-2 pb-2 pl-4 pr-4 text-neutral-300 bg-transparent">
             <Link href="/" passHref>
@@ -20,10 +38,11 @@ export const Navbar = () => {
             </Link>
             <div className="grow"></div>
             <div className="flex flex-row gap-3 justify-around items-center">
-                <p className="font-light text-base">Movies</p>
-                <p className="font-light text-base">TV Shows</p>
-                <p className="font-light text-base">People</p>
-                {/* <p className="font-light text-base">Log in</p> */}
+
+
+                <UserElement user={user} router={router} />
+
+
             </div>
         </nav>
     )
