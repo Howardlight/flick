5d29a5ea174b4d71e28e73f9bc4085675dca8096
Mhammad Nazir <nazirchase@gmail.com>
2023-09-14 13:38:59 +0300

    Revamped PopularTV

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import type { Metadata, NextPage } from 'next'
-import { PopularResponse } from '../types/GetPopularMoviesTypes';
 import { Navbar } from '../components/Navbar';
 import UpcomingMovies from '../components/Index/UpcomingMovies';
 import PopularTV from '../components/Index/PopularTV';
 import SearchBar from '../components/Index/SearchBar';
-import { Fragment, Suspense } from 'react';
-import { UpcomingResponse } from '../types/GetUpcomingTypes';
-import { GetPopularTV } from '../types/GetPopularTVTypes';
-import { IndexWidget, IndexWidgetSkeletons } from '../components/Index/IndexWidgetBase';
+import { Fragment } from 'react';
 import PopularMovies from '../components/Index/PopularMovies';
 
 
@@ -15,28 +11,7 @@ export const metadata: Metadata = {
   title: "Home - Flick"
 }
 
-async function getUpcomingMovies(page: number) {
-  const req = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${page}`,
-    { cache: "no-store" }
-  );
-  const data: UpcomingResponse = await req.json();
-
-  return data;
-}
-
-async function getPopularTV(page: number) {
-  const req = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${page}`,
-    { cache: "no-store" }
-  );
-  const data: GetPopularTV = await req.json();
-
-  return data;
-}
-
-
 export default async function Home() {
-  const upcomingMovies = await getUpcomingMovies(1);
-  const popularTV = await getPopularTV(1);
   //TODO: h-[50vh] does not seem to work for some reason, find out why and fix it
   //TODO: you cannot declare <head></head> inside the body of a server component, it must be declared in a layout
   // Port all Endpoints to their layout
@@ -52,10 +27,8 @@ export default async function Home() {
 
         <PopularMovies />
         <UpcomingMovies className='mt-10' />
+        <PopularTV className='mt-10' />
 
-        {/* <Suspense fallback={<IndexWidgetSkeletons />}>
-          <PopularTV className='mt-10' popularTV={popularTV} />
-        </Suspense> */}
       </main>
     </Fragment>
   )
--- components/Index/PopularTV.tsx
+++ components/Index/PopularTV.tsx
@@ -1,24 +1,40 @@
-"use client";
-
+import { Suspense } from "react";
 import { GetPopularTV, TVResult } from "../../types/GetPopularTVTypes";
-import { IndexWidget } from "./IndexWidgetBase";
+import { IndexWidget, IndexWidgetSkeletons } from "./IndexWidgetBase";
 
-export default function PopularTV({ className, popularTV }: { className: string, popularTV: GetPopularTV }) {
+async function getPopularTV(page: number) {
+    const req = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${page}`,
+        { cache: "no-store" }
+    );
+    const data: GetPopularTV = await req.json();
+
+    return data;
+}
+
+export default function PopularTV({ className }: { className: string }) {
     return (
         <IndexWidget className={`${className}`} title="Popular Shows" key={"popular-shows"}>
             <IndexWidget.Scrollbar>
-                {popularTV!.results.map((tv: TVResult) => {
-                    return (
-                        <IndexWidget.Wrapper title={tv.name} key={tv.id} mediaType="tv" resultID={tv.id}>
-                            <IndexWidget.Poster title={tv.name} url={tv.poster_path} />
-                            <div className='flex flex-col justify-end grow mt-2 max-w-[250px]'>
-                                <p className='font-medium text-lg ml-2 pb-2 text-gray-100 truncate'>{tv.name}</p>
-                                <IndexWidget.Metrics vote_average={tv.vote_average} />
-                            </div>
-                        </IndexWidget.Wrapper>
-                    )
-                })}
+                <Suspense fallback={<IndexWidgetSkeletons />}>
+                    <PopularTVContent />
+                </Suspense>
             </IndexWidget.Scrollbar>
         </IndexWidget>
     )
+}
+
+async function PopularTVContent() {
+    const popularTV = await getPopularTV(1);
+
+    return popularTV!.results.map((tv: TVResult) => {
+        return (
+            <IndexWidget.Wrapper title={tv.name} key={tv.id} mediaType="tv" resultID={tv.id}>
+                <IndexWidget.Poster title={tv.name} url={tv.poster_path} />
+                <div className='flex flex-col justify-end grow mt-2 max-w-[250px]'>
+                    <p className='font-medium text-lg ml-2 pb-2 text-gray-100 truncate'>{tv.name}</p>
+                    <IndexWidget.Metrics vote_average={tv.vote_average} />
+                </div>
+            </IndexWidget.Wrapper>
+        )
+    })
 }
