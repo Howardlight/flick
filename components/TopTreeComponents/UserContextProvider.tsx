131e2b4db513e124b0e56c5b4b98441627a0bc00
Mhammad Nazir <nazirchase@gmail.com>
2023-09-14 11:43:27 +0300

    Removed NProgress

--- components/TopTreeComponents/UserContextProvider.tsx
+++ components/TopTreeComponents/UserContextProvider.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { Router } from "next/router";
-import { createContext, ReactElement, useEffect } from "react";
+import { createContext } from "react";
 import { User } from "../../types/User";
 import useUser from "../../useUser";
-import NProgress from "nprogress";
 
 const defaultUserContext: User = {
     isLoggedIn: false
@@ -19,35 +17,9 @@ export default function ContextProvider({ children }: { children: React.ReactNod
     });
     // console.log("user: ", user);
 
-    //TODO: Fix NProgress
-    useTopLevelNProgressBar();
-
     return (
         <UserContext.Provider value={user!}>
             {children}
         </UserContext.Provider>
     )
-}
-
-export function useTopLevelNProgressBar() {
-
-    function useNProgressBar(router: Router) {
-        useEffect(() => {
-
-            router.events.on("routeChangeStart", (url) => {
-                NProgress.start()
-            });
-
-            router.events.on("routeChangeComplete", (url) => {
-                NProgress.done()
-            });
-
-            // Router.events.on("routeChangeError", (url) => {
-            //   NProgress.done()
-            // });
-
-        }, [router])
-    }
-    // ProgressBar when routing
-    useNProgressBar(Router as unknown as Router);
 }
