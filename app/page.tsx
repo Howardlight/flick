76cd883e62d15a8d9a11e653c2df4d76ef18d439
Mhammad Nazir <nazirchase@gmail.com>
2023-09-18 14:32:54 +0300

    Added Nicer Hero to Main Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,35 @@ import { Navbar } from '../components/Navbar';
 import UpcomingMovies from '../components/Index/UpcomingMovies';
 import PopularTV from '../components/Index/PopularTV';
 import SearchBar from '../components/Index/SearchBar';
-import { Fragment } from 'react';
 import PopularMovies from '../components/Index/PopularMovies';
-
 
 export const metadata: Metadata = {
   title: "Home - Flick"
 }
 
 export default async function Home() {
-  //TODO: h-[50vh] does not seem to work for some reason, find out why and fix it
-  // Port all Endpoints to their layout
+  //TODO: Port all Endpoints to their layout
 
   return (
-    <Fragment>
-      <Navbar />
-      <main className='bg-black mb-10'>
-        <div style={{ height: "50vh" }} className='flex flex-col justify-center bg-black'>
-          <p className="font-semibold text-neutral-100 self-center text-xl mb-5">All kinds of Shows that you&apos;ll enjoy</p>
-          <SearchBar />
+    <main className='bg-black mb-10'>
+      <div className='max-md:h-[500px] h-[700px] w-auto bg-hero'>
+        <div className='max-md:h-[500px] h-[700px] w-auto backdrop-blur-sm flex flex-col'>
+          <Navbar />
+
+          <div className='flex-grow flex flex-col justify-center '>
+            <p className="font-semibold text-neutral-100 self-center text-xl mb-5">All kinds of Shows that you&apos;ll enjoy</p>
+            <SearchBar />
+          </div>
+
         </div>
+      </div>
+      <div className='border-t-4 md:border-t-8 border-solid border-red-600'>
+
 
         <PopularMovies />
         <UpcomingMovies className='mt-10' />
         <PopularTV className='mt-10' />
-
-      </main>
-    </Fragment>
+      </div>
+    </main>
   )
 }
