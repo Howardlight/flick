601f0e38e95c6e061326abdbe74546a666bc5cd1
Mhammad Nazir <nazirchase@gmail.com>
2023-09-21 15:33:09 +0300

    Added TODOs

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -10,6 +10,15 @@ export const metadata: Metadata = {
 
 export default function SignIn() {
 
+    //TODO: Flesh this out properly
+    // Add a nice Background
+    // Add a nice top Header
+    // Mention this registration being by TMDB
+    // Link TMDB and their registration
+    //TODO: Handle Errors more swiftly
+    //TODO: Fix Error that occurs with hydration, specifically with form appearing new div
+    //TODO: Make form Responsive to bigger Screens, looks ok for small screens, needs alot of work
+
     return (
         <div className="h-screen w-auto p-10">
             <div className="flex flex-row items-center">
