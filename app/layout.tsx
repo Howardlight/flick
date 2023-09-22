f4f1787cfb9f413dc755c1d587688cafa91c1cc4
Mhammad Nazir <nazirchase@gmail.com>
2023-09-22 11:28:31 +0300

    Added Favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,16 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     return (
         <html lang="en">
             <head>
-                {/* <title>Flick - Home</title> */}
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+                <meta name="apple-mobile-web-app-title" content="Flick" />
+                <meta name="application-name" content="Flick" />
+                <meta name="msapplication-TileColor" content="#2d89ef" />
+                <meta name="theme-color" content="#000000" />
+
                 <meta name="description" content="Explore the latest and most popular movies using Flick! Discover details never heard of about your favorite actors! Find your next favorite TV Show using Project Movies!" />
                 <link rel="canonical" href="https://flick.vercel.app" />
 
