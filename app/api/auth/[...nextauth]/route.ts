4c1d2d8ed7f484aa5489f647f165562f1c933f3f
Mhammad Nazir <nazirchase@gmail.com>
2023-09-24 19:56:03 +0300

    Imported Session and logError in nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
-import type { AuthOptions } from "next-auth"
+import type { AuthOptions, Session } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import { logError } from "../../../../utils"
 
 interface RequestToken {
     success: boolean,
