e845e2ddcc5c51ed25e47e4436a0838b93f9706f
Mhammad Nazir <nazirchase@gmail.com>
2023-09-25 19:16:54 +0300

    Safe guarded PopularMovies

--- components/Index/PopularMovies.tsx
+++ components/Index/PopularMovies.tsx
@@ -1,14 +1,20 @@
 import { Suspense } from 'react';
 import { PopularResponse, PopularResult } from '../../types/GetPopularMoviesTypes';
 import { IndexWidget, IndexWidgetSkeletons } from './IndexWidgetBase';
+import { logError } from '../../utils';
 
 async function getPopularMovies(page: number) {
-  const req = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${page}`,
-    { cache: "no-store" }
-  );
-  const data: PopularResponse = await req.json();
+  try {
+    const req = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=${page}`,
+      { cache: "no-store" }
+    );
+    const data: PopularResponse = await req.json();
 
-  return data;
+    return data;
+  } catch (error: any) {
+    logError(error, "getPopularMovies");
+    return null;
+  }
 }
 
 export default function PopularMovies() {
@@ -27,17 +33,26 @@ async function PopularMoviesContent() {
   const popularMovies = await getPopularMovies(1);
 
 
-  return popularMovies!.results.map((item: PopularResult) => {
-    return (
-      <IndexWidget.Wrapper title={item.title} mediaType='movie' resultID={item.id} key={`popular-movies-${item.id}`}>
-        <IndexWidget.Poster title={item.title} url={item.poster_path} />
 
-        <div className='flex flex-col grow justify-end mt-2 max-w-[250px]'>
-          <p className='font-medium text-lg ml-2 pb-2 text-gray-100 truncate'>{item.title}</p>
-          <IndexWidget.Metrics vote_average={item.vote_average} />
-        </div>
+  try {
+    return popularMovies!.results.map((item: PopularResult) => {
+      return (
+        <IndexWidget.Wrapper title={item.title} mediaType='movie' resultID={item.id} key={`popular-movies-${item.id}`}>
+          <IndexWidget.Poster title={item.title} url={item.poster_path} />
 
-      </IndexWidget.Wrapper>
-    );
-  })
+          <div className='flex flex-col grow justify-end mt-2 max-w-[250px]'>
+            <p className='font-medium text-lg ml-2 pb-2 text-gray-100 truncate'>{item.title}</p>
+            <IndexWidget.Metrics vote_average={item.vote_average} />
+          </div>
+
+        </IndexWidget.Wrapper>
+      );
+    });
+
+
+
+  } catch (error: any) {
+    logError(error, "<PopularMoviesContent />")
+    return <p>Could not fetch Popular Movies</p>
+  }
 }
